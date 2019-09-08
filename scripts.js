efa0bfa047dbb204581c5eb72aca390a299b9c38
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-09-08 15:39:44 -0500

    worked on styling

--- scripts.js
+++ scripts.js
@@ -18,14 +18,14 @@ btn.addEventListener('click', function () {
       let lineA = document.createElement('p');
       lineA.textContent = (x + ' line of code in the file, ' + x + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file.')
       if (x === 1) {
-        friendHeading.appendChild(lineA);
+        div.appendChild(lineA);
         //  console.log(x + ' line of code in the file, ' + x + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
       } else {
         let next = (x - 1);
         let lineB = document.createElement('p');
-        lineB.textContent = (x + ' lines of code in the file, ' + x + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + next + ' lines of code in the file');
+        lineB.textContent = (x + ' lines of code in the file, ' + x + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + next + ' lines of code in the file.');
         
-        friendHeading.appendChild(lineB);
+        div.appendChild(lineB);
         //  console.log(x + ' lines of code in the file, ' + x + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + next + ' lines of code in the file');
       }
     }
