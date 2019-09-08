7a56708243925ed7badb7bd7890432add6d30a0f
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-09-08 15:31:14 -0500

    added a button and an event that creates 5 divs, each of a friend, with a heading and lines to the song.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,33 @@
+let btn = document.getElementById('sing');
 
 
-let friends = ['Jesse', 'Clark', 'Dub', 'Jake', 'Jonathan'];
+btn.addEventListener('click', function () {
 
-for (let i=0; i < friends.length; i++) {
-  console.log(friends[i].toUpperCase() + ':');
-  for (let x=99; x >= 1; x--) {
-   if (x === 1) {
-     console.log(x + ' line of code in the file, ' + x + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
-   } else {
-     let next = (x - 1);
-     console.log(x + ' lines of code in the file, ' + x + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + next + ' lines of code in the file');
-   }
+  let friends = ['Jesse', 'Clark', 'Dub', 'Jake', 'Jonathan'];
+
+  for (let i = 0; i < friends.length; i++) {
+    
+    let div = document.createElement('div');
+    div.classList.add('friend');
+    let friendHeading = document.createElement('h3');
+    friendHeading.textContent = friends[i].toUpperCase() + ':';
+    div.appendChild(friendHeading);
+    document.body.appendChild(div);
+    // console.log(friends[i].toUpperCase() + ':');
+    for (let x = 99; x >= 1; x--) {
+      let lineA = document.createElement('p');
+      lineA.textContent = (x + ' line of code in the file, ' + x + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file.')
+      if (x === 1) {
+        friendHeading.appendChild(lineA);
+        //  console.log(x + ' line of code in the file, ' + x + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
+      } else {
+        let next = (x - 1);
+        let lineB = document.createElement('p');
+        lineB.textContent = (x + ' lines of code in the file, ' + x + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + next + ' lines of code in the file');
+        
+        friendHeading.appendChild(lineB);
+        //  console.log(x + ' lines of code in the file, ' + x + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + next + ' lines of code in the file');
+      }
+    }
   }
-}
+});
